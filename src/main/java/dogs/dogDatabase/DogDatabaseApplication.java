728eb65feb133974f580bc897a4d49323227ec43
Suvi Keränen <suvi.keranen@outlook.com>
2023-09-27 19:29:39 +0300

    Initial commit

---
+++ src/main/java/dogs/dogDatabase/DogDatabaseApplication.java
@@ -0,0 +1,13 @@
+package dogs.dogDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dogs/dogDatabase/DogDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dogs.dogDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
